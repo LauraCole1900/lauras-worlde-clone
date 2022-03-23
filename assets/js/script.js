a2ee22317561fcbbc4b114a219b5c83f879adef6
Laura Cole <lauracole1900@comcast.net>
2022-03-22 21:38:26 -0600

    broke keyboard into 3 rows so it renders correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,19 @@ $(function () {
 
   const wordBank = ["meter", "blues", "drums", "voice", "vocal", "choir", "viola", "baton", "verdi", "haydn", "basie", "elvis", "notes", "beats", "piano", "organ", "flute", "tenor", "altos", "genre", "pedal", "motif", "motet", "conga", "dance", "cello", "music", "whole", "forte", "mezzo", "largo", "lento", "tempo", "mosso", "segno", "ossia", "tacet", "tutti", "rests", "dolce", "senza", "staff", "clefs", "canon", "sharp", "grave", "minor", "major", "modal", "rondo", "trill", "tonal", "basso", "lyric", "elegy", "etude", "fugue", "legno", "octet", "pitch", "scale", "third", "fifth", "sixth", "banjo", "bugle", "carol", "chord", "tonic", "drone", "duple", "chant", "kazoo", "march", "opera", "pluck", "strum", "pulse", "range", "brass", "reeds", "score", "sitar", "snare", "swing", "theme", "triad", "verse", "waltz", "polka", "suite", "tuner", "indie", "liszt", "ravel", "satie", "elgar", "grieg", "bizet", "davis", "louis", "sousa", "ditty", "dylan", "berry", "samba", "rumba", "slide", "gliss", "canto", "parte", "prima", "segue", "count", "audio", "forza", "stand", "intro", "molto", "recit", "quasi", "breve", "shake", "sopra", "sotto", "tasto", "corda", "break", "chops", "track", "click", "combo", "cover", "drive", "stage", "house", "front", "valve", "remix", "shred"]
 
-  const keyboardChar = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+  const keyboardChar = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+
+  let letterEl;
 
   keyboardChar.map((char, i) => {
     letterEl = $("<div>").text(char.toUpperCase()).addClass("letterKey centered").attr("id", char).on("click", addLetter)
-    if (i === 10) {
-      letterEl.addClass("newRow")
+    if (i < 10) {
+      $("#row1").append(letterEl);
+    } else if (i >= 10 && i < 19) {
+      $("#row2").append(letterEl);
+    } else {
+      $("#row3").append(letterEl);
     }
-    $("#keyboard").append(letterEl);
   })
 
   function addLetter(e) {
