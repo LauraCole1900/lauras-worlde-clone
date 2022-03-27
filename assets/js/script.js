1fd06336434bb6b96299432e160071662c1f2fbc
Laura Cole <lauracole1900@comcast.net>
2022-03-26 23:09:49 -0600

    changed userGuess variables to objects to track length & submission status; added getWord()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,46 @@
 $(function () {
 
-  const wordBank = ["ossia", "brass", "basie", "stage", "triad", "indie", "notes", "major", "satie", "sotto", "scale", "conga", "prima", "trill", "bizet", "genre", "piano", "combo", "elgar", "grave", "cover", "tenor", "recit", "elvis", "pitch", "liszt", "pulse", "segue", "march", "dylan", "rondo", "baton", "senza", "davis", "dolce", "drive", "miles", "rests", "sharp", "elegy", "mosso", "etude", "music", "verdi", "tempo", "bugle", "beats", "snare", "choir", "parte", "tutti", "legno", "break", "slide", "samba", "forza", "sousa", "stand", "click", "cello", "audio", "lento", "front", "tonic", "breve", "meter", "range", "basso", "count", "motet", "carol", "tasto", "waltz", "blues", "modal", "rumba", "dance", "chant", "strum", "chord", "chops", "largo", "grieg", "canon", "viola", "clefs", "suite", "remix", "molto", "forte", "staff", "tonal", "motif", "segno", "ditty", "block", "verse", "haydn", "sopra", "fugue", "corda", "valve", "drone", "sixth", "gliss", "flute", "shred", "opera", "swing", "quasi", "berry", "banjo", "third", "lyric", "ravel", "reeds", "mezzo", "house", "canto", "track", "duple", "intro", "altos", "score", "drums", "fifth", "sitar", "theme", "vocal", "kazoo", "polka", "pedal", "pluck", "tuner", "organ", "louis", "whole", "shake", "neume", "voice", "chuck", "minor", "tacet", "octet", "hogan"]
-
-  14, 28, 42, 56, 70, 84, 98, 112, 126, 140
+  const wordBank = ["ossia", "brass", "basie", "stage", "triad", "indie", "notes", "major", "satie", "sotto", "scale", "conga", "prima", "trill", "bizet", "genre", "piano", "combo", "elgar", "grave", "cover", "tenor", "recit", "elvis", "pitch", "liszt", "pulse", "segue", "march", "dylan", "rondo", "baton", "senza", "davis", "dolce", "drive", "miles", "rests", "sharp", "elegy", "mosso", "etude", "music", "verdi", "tempo", "bugle", "beats", "snare", "choir", "parte", "tutti", "legno", "break", "slide", "samba", "forza", "sousa", "stand", "click", "cello", "audio", "lento", "front", "tonic", "breve", "meter", "range", "basso", "count", "motet", "carol", "tasto", "waltz", "blues", "modal", "rumba", "dance", "chant", "strum", "chord", "chops", "largo", "grieg", "canon", "viola", "clefs", "suite", "remix", "molto", "forte", "staff", "tonal", "motif", "segno", "ditty", "block", "verse", "haydn", "sopra", "fugue", "corda", "valve", "drone", "sixth", "gliss", "flute", "shred", "opera", "swing", "quasi", "berry", "banjo", "third", "lyric", "ravel", "reeds", "mezzo", "house", "canto", "track", "duple", "intro", "altos", "score", "drums", "fifth", "sitar", "theme", "vocal", "kazoo", "polka", "pedal", "pluck", "tuner", "organ", "louis", "whole", "shake", "neume", "voice", "chuck", "minor", "tacet", "octet", "hogan"];
 
   const keyboardChar = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "Enter", "z", "x", "c", "v", "b", "n", "m", "Backspace"];
 
   let letterEl;
   let guessEl;
-  let userGuess1 = [];
-  let userGuess2 = [];
-  let userGuess3 = [];
-  let userGuess4 = [];
-  let userGuess5 = [];
-  let userGuess6 = [];
+  let word = "";
+  let userGuess1 = {
+    letters: [],
+    submit: false
+  };
+  let userGuess2 = {
+    letters: [],
+    submit: false
+  };
+  let userGuess3 = {
+    letters: [],
+    submit: false
+  };
+  let userGuess4 = {
+    letters: [],
+    submit: false
+  };
+  let userGuess5 = {
+    letters: [],
+    submit: false
+  };
+  let userGuess6 = {
+    letters: [],
+    submit: false
+  };
+  const startDate = new Date("3/26/2022");
+  getWord(startDate);
+
+  function getWord(date) {
+    const todayDate = Date.now();
+    const timeDiff = todayDate - date.getTime();
+    const dayDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+    word = wordBank.at(dayDiff);
+    console.log({ word });
+  }
 
   keyboardChar.map((char, i) => {
     letterEl = $("<div>").text(char.toUpperCase()).addClass("letterKey centered").attr("id", char).on("click", addLetter)
